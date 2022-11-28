7e011b25af40cc90df95f299635c5cdd8627b9af
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-28 15:32:59 -0300

    Finished the receiver

--- src/gato_receiver/gnuradio/top_block.py
+++ src/gato_receiver/gnuradio/top_block.py
@@ -81,27 +81,44 @@ class top_block(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 2560000
-        self.channel_width = channel_width = 200e3
+        self.resampler_interp = resampler_interp = 12
+        self.resampler_decimation = resampler_decimation = 5
+        self.quadrature = quadrature = 250e3
+        self.lp_width = lp_width = 25e3
+        self.lp_decimation = lp_decimation = 12
+        self.lp_cutoff = lp_cutoff = 75e3
         self.channel_freq = channel_freq = 93300000
-        self.center_freq = center_freq = 93300000
         self.audio_gain = audio_gain = 1
 
         ##################################################
         # Blocks
         ##################################################
-        self._channel_freq_range = Range(50000000, 150000000, 1, 93300000, 200)
+        self._quadrature_range = Range(1e3, 500e3, 1e3, 250e3, 200)
+        self._quadrature_win = GrRangeWidget(self._quadrature_range, self.set_quadrature, "Quadrature", "counter_slider", float, QtCore.Qt.Horizontal, "value")
+
+        self.top_layout.addWidget(self._quadrature_win)
+        self._lp_width_range = Range(1e3, 300e3, 1e3, 25e3, 200)
+        self._lp_width_win = GrRangeWidget(self._lp_width_range, self.set_lp_width, "Low pass width", "counter_slider", float, QtCore.Qt.Horizontal, "value")
+
+        self.top_layout.addWidget(self._lp_width_win)
+        self._lp_cutoff_range = Range(1e3, 300e3, 1e3, 75e3, 200)
+        self._lp_cutoff_win = GrRangeWidget(self._lp_cutoff_range, self.set_lp_cutoff, "Low pass cutoff", "counter_slider", float, QtCore.Qt.Horizontal, "value")
+
+        self.top_layout.addWidget(self._lp_cutoff_win)
+        self._channel_freq_range = Range(50000000, 150000000, 100000, 93300000, 200)
         self._channel_freq_win = GrRangeWidget(self._channel_freq_range, self.set_channel_freq, "Channel frequency", "counter_slider", int, QtCore.Qt.Horizontal, "value")
 
         self.top_layout.addWidget(self._channel_freq_win)
-        self._audio_gain_range = Range(0, 100, 1e-6, 1, 200)
+        self._audio_gain_range = Range(0, 100, 0.1, 1, 200)
         self._audio_gain_win = GrRangeWidget(self._audio_gain_range, self.set_audio_gain, "Audio Gain", "counter_slider", float, QtCore.Qt.Horizontal, "value")
 
         self.top_layout.addWidget(self._audio_gain_win)
         self.rtlsdr_source_0 = osmosdr.source(
             args="numchan=" + str(1) + " " + ""
         )
+        self.rtlsdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.rtlsdr_source_0.set_sample_rate(samp_rate)
-        self.rtlsdr_source_0.set_center_freq(center_freq, 0)
+        self.rtlsdr_source_0.set_center_freq(channel_freq, 0)
         self.rtlsdr_source_0.set_freq_corr(0, 0)
         self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
         self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
@@ -111,17 +128,73 @@ class top_block(gr.top_block, Qt.QWidget):
         self.rtlsdr_source_0.set_bb_gain(20, 0)
         self.rtlsdr_source_0.set_antenna('', 0)
         self.rtlsdr_source_0.set_bandwidth(0, 0)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=12,
-                decimation=5,
+        self._resampler_interp_range = Range(1, 50, 1, 12, 200)
+        self._resampler_interp_win = GrRangeWidget(self._resampler_interp_range, self.set_resampler_interp, "Resampler interpolation", "counter_slider", int, QtCore.Qt.Horizontal, "value")
+
+        self.top_layout.addWidget(self._resampler_interp_win)
+        self._resampler_decimation_range = Range(1, 50, 1, 5, 200)
+        self._resampler_decimation_win = GrRangeWidget(self._resampler_decimation_range, self.set_resampler_decimation, "Resampler decimation", "counter_slider", int, QtCore.Qt.Horizontal, "value")
+
+        self.top_layout.addWidget(self._resampler_decimation_win)
+        self.rational_resampler_xxx_1 = filter.rational_resampler_fff(
+                interpolation=int(48e3),
+                decimation=int(quadrature),
                 taps=[],
                 fractional_bw=0)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            1024, #size
+            samp_rate, #samp_rate
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_freq_sink_x_0_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             channel_freq, #fc
             samp_rate, #bw
-            "", #name
+            "Filtered entrance", #name
             1,
             None # parent
         )
@@ -161,9 +234,9 @@ class top_block(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
-            center_freq, #fc
+            channel_freq, #fc
             samp_rate, #bw
-            "", #name
+            "SDR source", #name
             1,
             None # parent
         )
@@ -200,36 +273,37 @@ class top_block(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self._lp_decimation_range = Range(1, 50, 1, 12, 200)
+        self._lp_decimation_win = GrRangeWidget(self._lp_decimation_range, self.set_lp_decimation, "Low pass decimation", "counter_slider", int, QtCore.Qt.Horizontal, "value")
+
+        self.top_layout.addWidget(self._lp_decimation_win)
         self.low_pass_filter_0 = filter.fir_filter_ccf(
-            (int(samp_rate / channel_width)),
+            (int(samp_rate / quadrature)),
             firdes.low_pass(
                 1,
                 samp_rate,
-                20000,
-                1000,
+                lp_cutoff,
+                lp_width,
                 window.WIN_HAMMING,
                 6.76))
-        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(audio_gain)
-        self.audio_sink_0 = audio.sink(44100, '', True)
+        self.audio_sink_0 = audio.sink(48000, '', True)
         self.analog_wfm_rcv_0 = analog.wfm_rcv(
-        	quad_rate=480000,
-        	audio_decimation=10,
+        	quad_rate=quadrature,
+        	audio_decimation=1,
         )
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, (center_freq - channel_freq), 1, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.analog_wfm_rcv_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_1, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.qtgui_freq_sink_x_0_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
-        self.connect((self.rtlsdr_source_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.rational_resampler_xxx_1, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.rtlsdr_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
 
 
@@ -246,34 +320,58 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 20000, 1000, window.WIN_HAMMING, 6.76))
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.center_freq, self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.lp_cutoff, self.lp_width, window.WIN_HAMMING, 6.76))
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.channel_freq, self.samp_rate)
         self.qtgui_freq_sink_x_0_0.set_frequency_range(self.channel_freq, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
 
-    def get_channel_width(self):
-        return self.channel_width
+    def get_resampler_interp(self):
+        return self.resampler_interp
 
-    def set_channel_width(self, channel_width):
-        self.channel_width = channel_width
+    def set_resampler_interp(self, resampler_interp):
+        self.resampler_interp = resampler_interp
+
+    def get_resampler_decimation(self):
+        return self.resampler_decimation
+
+    def set_resampler_decimation(self, resampler_decimation):
+        self.resampler_decimation = resampler_decimation
+
+    def get_quadrature(self):
+        return self.quadrature
+
+    def set_quadrature(self, quadrature):
+        self.quadrature = quadrature
+
+    def get_lp_width(self):
+        return self.lp_width
+
+    def set_lp_width(self, lp_width):
+        self.lp_width = lp_width
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.lp_cutoff, self.lp_width, window.WIN_HAMMING, 6.76))
+
+    def get_lp_decimation(self):
+        return self.lp_decimation
+
+    def set_lp_decimation(self, lp_decimation):
+        self.lp_decimation = lp_decimation
+
+    def get_lp_cutoff(self):
+        return self.lp_cutoff
+
+    def set_lp_cutoff(self, lp_cutoff):
+        self.lp_cutoff = lp_cutoff
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.lp_cutoff, self.lp_width, window.WIN_HAMMING, 6.76))
 
     def get_channel_freq(self):
         return self.channel_freq
 
     def set_channel_freq(self, channel_freq):
         self.channel_freq = channel_freq
-        self.analog_sig_source_x_0.set_frequency((self.center_freq - self.channel_freq))
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.channel_freq, self.samp_rate)
         self.qtgui_freq_sink_x_0_0.set_frequency_range(self.channel_freq, self.samp_rate)
-
-    def get_center_freq(self):
-        return self.center_freq
-
-    def set_center_freq(self, center_freq):
-        self.center_freq = center_freq
-        self.analog_sig_source_x_0.set_frequency((self.center_freq - self.channel_freq))
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.center_freq, self.samp_rate)
-        self.rtlsdr_source_0.set_center_freq(self.center_freq, 0)
+        self.rtlsdr_source_0.set_center_freq(self.channel_freq, 0)
 
     def get_audio_gain(self):
         return self.audio_gain
